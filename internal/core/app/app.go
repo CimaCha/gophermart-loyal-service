f774a7d151a9716db23fcbfb3faa2d169bc4d2bc
Sayf4rgo <sayf4rgo@gmail.com>
2026-09-22 16:05:09 +0500

    добавлен mockery.yaml в каждый сервис + middleware разделены на отдельные *.go файлы

--- internal/core/app/app.go
+++ internal/core/app/app.go
@@ -6,10 +6,8 @@ import (
 	"log/slog"
 
 	"github.com/CimaCha/gophermart-loyal-service/internal/config"
-	"github.com/CimaCha/gophermart-loyal-service/internal/core/auth/jwtoken"
 	"github.com/CimaCha/gophermart-loyal-service/internal/core/db/postgres"
 	"github.com/CimaCha/gophermart-loyal-service/internal/core/httpserver"
-	"github.com/CimaCha/gophermart-loyal-service/internal/core/transport/http/middleware"
 	orderh "github.com/CimaCha/gophermart-loyal-service/internal/order/handler"
 	orderrepo "github.com/CimaCha/gophermart-loyal-service/internal/order/repository"
 	ordersvc "github.com/CimaCha/gophermart-loyal-service/internal/order/service"
@@ -36,7 +34,6 @@ func New(cfg *config.Config, log *slog.Logger) (*App, error) {
 	httpServer := httpserver.New(rootRouter, cfg.Server, log)
 
 	// Потом убрать в .env
-	tokenService := jwtoken.NewTokenService([]byte("our-secret-key"))
 
 	// userRepo
 	orderRepo := orderrepo.New(pool)
@@ -52,7 +49,6 @@ func New(cfg *config.Config, log *slog.Logger) (*App, error) {
 
 	// Регаем приватные маршруты для order handler
 	rootRouter.Group(func(r chi.Router) {
-		r.Use(middleware.Auth(tokenService, log))
 
 		orderHandler.RegisterPrivateAPI(r)
 	})
--- internal/core/transport/http/middleware/middleware.go
+++ internal/core/transport/http/middleware/middleware.go
@@ -1,115 +1,12 @@
 package middleware
 
 import (
-	"log/slog"
 	"net/http"
-	"strings"
-	"time"
-
-	"github.com/CimaCha/gophermart-loyal-service/internal/core/auth/jwtoken"
-	"github.com/CimaCha/gophermart-loyal-service/internal/core/transport/http/ctxkeys"
-	httpresponse "github.com/CimaCha/gophermart-loyal-service/internal/core/transport/http/response"
-	"github.com/google/uuid"
 )
 
 type Middleware func(http.Handler) http.Handler
-
-type TokenValidator interface {
-	ValidateToken(tokenString string) (*jwtoken.Claims, error)
-}
 
 const (
 	auth          = "Authorization"
 	requestHeader = "X-Request-ID"
 )
-
-// Просто проставляет в Header X-Request-ID, чтобы можно было логировать цепочку запросов
-func RequestID() Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestID := r.Header.Get(requestHeader)
-			if requestID == "" {
-				requestID = uuid.NewString()
-			}
-
-			r.Header.Set(requestHeader, requestID)
-			w.Header().Set(requestHeader, requestID)
-
-			next.ServeHTTP(w, r)
-		})
-	}
-}
-
-func Logging(log *slog.Logger) Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestid := r.Header.Get(requestHeader)
-			rw := httpresponse.New(w)
-			start := time.Now()
-
-			l := log.With(
-				slog.String("request_id", requestid),
-				slog.String("user_agent", r.Header.Get("User-Agent")),
-				slog.String("URI", r.RequestURI),
-				slog.String("method", r.Method),
-			)
-
-			l.Info("HTTP Request started")
-
-			next.ServeHTTP(rw, r)
-
-			duration := time.Since(start)
-
-			l.Info(
-				"HTTP Request done",
-				slog.Int("status", rw.GetStatusCode()),
-				slog.Duration("latency", duration),
-				slog.Int("size", rw.GetBodySize()),
-			)
-		})
-	}
-}
-
-func Auth(validator TokenValidator, log *slog.Logger) Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-			authHeader := r.Header.Get(auth)
-			if authHeader == "" {
-
-				return
-			}
-
-			parts := strings.SplitN(authHeader, " ", 2)
-			if len(parts) != 2 || parts[0] != "Bearer" {
-
-				return
-			}
-
-			tokenString := parts[1]
-
-			claims, err := validator.ValidateToken(tokenString)
-
-			if err != nil {
-				log.Info(
-					"token validation failed",
-					"err", err,
-				)
-				http.Error(
-					w,
-					"invalid or expiration token",
-					http.StatusUnauthorized,
-				)
-				return
-			}
-
-			log.Info(
-				"user authenticated",
-			)
-
-			ctx := ctxkeys.WithUserID(r.Context(), claims.UserID)
-
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
-}
